d0cef1be0ffb4556f1bc9c20477d1f453acf01dc
Anatoliy <furmankevyhc@ukr.net>
2023-05-10 06:28:26 +0200

    added js, changed html css

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 "use strict";
 // add to cart
+
+// for (let i = 0; i < buttonInBetweenForm.length; i++) {
+//   buttonInBetweenForm[i].addEventListener("cklick", function (e) {
+//     console.log("clicked");
+//   });
+// }
+
+// prevProductsCouns = +productsCountEl.textContent;
+//     console.log(prevProductsCouns);
+//     productsCountEl.textContent = prevProductsCouns + 1;
+
 let productsCountEl = document.getElementById("products-count");
-console.log(productsCountEl);
 
-let addToCartBtns = document.querySelectorAll(".button-inBetweenForm-2");
-console.log(addToCartBtns);
+let buttonInBetweenForm = document.querySelectorAll(".button-inBetweenForm");
 
-for (let i = 0; i < addToCartBtns.length; i++) {
-  addToCartBtns[i].addEventListener("click", function () {
-    console.log("clicked");
+buttonInBetweenForm.forEach((Item) => {
+  Item.addEventListener("click", function () {
+    productsCountEl.textContent = +productsCountEl.textContent + 1;
   });
-}
+});
+
+let likeBtns = document.querySelectorAll(".div-img-corect-heart");
+console.log(likeBtns);
+
+likeBtns.forEach((item) => {
+  item.addEventListener("click", function () {
+    if (item.classList.contains("liked")) {
+      item.classList.remove("liked");
+    } else {
+      item.classList.add("liked");
+    }
+  });
+});
+
+// change like state
+// likeBtns.forEach((item) => {
+//   item.addEventListener("click", function () {
+//     item.classList.toggle("licked");
+//   });
+// });
+// if (item.classList.contains("liked")) {
+//   item.classList.remove("liked");
+// } else {
+//   item.classList.add("liked");
+// }
+
+// let likeBtns = document.querySelectorAll(".like");
+// console.log(likeBtns);
+
+// item.classList.remove("liked");
