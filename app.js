b7738908f54f8028edc5df993652bc2527447536
Anatoliy <furmankevyhc@ukr.net>
2023-05-12 04:21:48 +0200

    added folder slick, added slick for html , css

--- app.js
+++ app.js
@@ -23,14 +23,15 @@ buttonInBetweenForm.forEach((Item) => {
 
 let likeBtns = document.querySelectorAll(".div-img-corect-heart");
 console.log(likeBtns);
-
+// change like state
 likeBtns.forEach((item) => {
   item.addEventListener("click", function () {
-    if (item.classList.contains("liked")) {
-      item.classList.remove("liked");
-    } else {
-      item.classList.add("liked");
-    }
+    item.classList.toggle("liked");
+    // if (item.classList.contains("liked")) {
+    //   item.classList.remove("liked");
+    // } else {
+    //   item.classList.add("liked");
+    // }
   });
 });
 
@@ -50,3 +51,45 @@ likeBtns.forEach((item) => {
 // console.log(likeBtns);
 
 // item.classList.remove("liked");
+
+//more details
+let moreDetails = document.querySelectorAll(".button-inBetweenForm-1");
+let modal = document.querySelector(".modal");
+moreDetails.forEach((item) => {
+  item.addEventListener("click", openModal);
+});
+
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+}
+function closeModal() {
+  modal.classList.remove("show");
+  modal.classList.add("hide");
+}
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+let moreForms = document.querySelectorAll(".button-inBetweenForm-1");
+let form = document.querySelector(".form");
+moreDetails.forEach((item) => {
+  item.addEventListener("click", openModal);
+});
+form.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(e.target);
+});
+
+//через function
+// item.addEventListener("click", openModal)
+// })
+// function openModal () {
+// modal.classList.add("show");
+// modal.classList.remove("hide");
+// }
+
+//slick
+$(".slider").slick();
